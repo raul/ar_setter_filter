708c383ecd3400662238759c23a9b17ec1053f83
Raul Murciano <raul@murciano.net>
2009-02-23 18:12:49 +0100

    Filters only applied to content columns: no id, xxx_id, etc

--- lib/ar_setter_filter.rb
+++ lib/ar_setter_filter.rb
@@ -9,17 +9,13 @@ module ActiveRecord
     module ClassMethods
 
       def setter_filter(filters, filtered_fields = nil)
-        filtered_fields ||= column_names_except_id
+        filtered_fields ||= content_columns.collect{ |col| col.name }
         filtered_fields.each do |field|
           define_method("#{field.to_s}=") do |new_value|
             filters.each{ |filter| new_value = send(filter, field, new_value) }
             write_attribute field, new_value
           end
         end
-      end
-      
-      def column_names_except_id
-        self.columns.collect{ |column| column.name } - ['id']
       end
       
     end # ClassMethods
